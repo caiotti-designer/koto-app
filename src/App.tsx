6371707eb9085b7aa26043d74303cc0d5581c2b1
CaioGustus <caioliutti@gmail.com>
2025-10-21 12:31:30 -0300

    chore: cleanup unused files and deps; simplify App; update package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
 import { useMemo } from 'react';
-import { Container } from './settings/types';
 import KotoDashboard from './components/generated/KotoDashboard';
-
 import { Toaster } from 'sonner';
 
-let container: Container = 'none';
-
 function App() {
-
-  const generatedComponent = useMemo(() => {
-    // THIS IS WHERE THE TOP LEVEL GENRATED COMPONENT WILL BE RETURNED!
-    return <KotoDashboard />; // %EXPORT_STATEMENT%
-  }, []);
-
-  if (container === 'centered') {
-    return (
-      <>
-        <div className="h-full w-full flex flex-col items-center justify-center">
-          {generatedComponent}
-        </div>
-        <Toaster position="top-center" richColors />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div style={{ position: 'absolute', top: '10px', left: '10px', zIndex: 100000 }}>
-    
-        </div>
-        {generatedComponent}
-        <Toaster position="top-center" richColors />
-      </>
-    );
-  }
+  const generatedComponent = useMemo(() => <KotoDashboard />, []);
+  return (
+    <>
+      {generatedComponent}
+      <Toaster position="top-center" richColors />
+    </>
+  );
 }
 
 export default App;
